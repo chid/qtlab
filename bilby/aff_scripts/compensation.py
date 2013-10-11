b09b035f6034bebba0756e61ad4ccd01224f2c02
Charley Peng <cpeng92@gmail.com>
2013-10-11 15:17:26 +1100

    Small Fixes.

--- bilby/aff_scripts/compensation.py
+++ bilby/aff_scripts/compensation.py
@@ -9,14 +9,7 @@
 from numpy import pi, random, arange, size
 from time import time, sleep
 import sys
-
 import qt
-
-print 'This is incomplete'
-import sys
-# probably needs some more try catches
-sys.exit(1)
-
 
 # filename and data tags
 # we still want to measure data of course.
@@ -34,15 +27,8 @@ I_sens = -1e-9  # A/V
 I_max = 1e-9    # A
 enable_protection = True
 
-# SOURCE_1 sweep
-V1_start = 0.00
-V1_end = 1.0
-V1_step = 0.005
-
-# SOURCE_2 sweep
-V2_start = 0.001
-V2_end = 0.001
-V2_step = 0.05
+V_start = 0.04
+V = V_start
 
 # Alternatively Set Sweep Time in Minutes
 T_sweep = True  # flag to pick time sweep
@@ -70,21 +56,15 @@ MULTIMETER_resolution = 3e-6
 # DON'T EDIT BELOW
 #
 
-def compensation(OldV=V, LockInThresh=0.005, Meter=None, MeterThresh=1e-8, LockIn=LOCKIN, jump=0.004, iterations=1):
-    """
-    @type LockIn: x_SR830
-    :param LockInThresh:
-    :param Meter:
-    :param MeterThresh:
-    :param LockIn:
-    :param jump:
-    :param iterations:
-    :return:
-    """
+def compensation(OldV=V, LockInThresh=0.005, Meter=None, MeterThresh=1e-8, LockIn=LOCKIN, jump=0.0004, iterations=1):
     # does one shot of compensation by default
     # threshold
 
     # TODO: Fix
+    # right now this will just be a hardcoded value
+    if OldV > .5 or OldV < 0:
+        print 'Outside of voltage range'
+        sys.exit(1)
 
     # Assumptions
     # All instruments initialized
@@ -107,19 +87,20 @@ def compensation(OldV=V, LockInThresh=0.005, Meter=None, MeterThresh=1e-8, LockI
                 OldV += jump # in volts
             else:
                 OldV -= jump
-        # else we do nothing.
+                # else we do nothing.
 
     # By default we do not measure I_sd but if it is provided we
     # also provide meter threshold. also this can be used just as a check we're on the peak
 
-    return OldV # which has now been modified
+    return OldV, LockInX  # which has now been modified
+
 
 # Function to check whether an instrument is initialized
-def check_instruments(str):
+def check_instruments(value):
     instruments = qt.instruments.get_instrument_names()
     flag = False
     for i in range(size(instruments)):
-        if instruments[i] == str:
+        if instruments[i] == value:
             flag = True
     return flag
 
@@ -178,27 +159,13 @@ if not check_instruments('LOCKIN'):
 # else we shouldn't need to do anything, though we could set frequency /
 # amplitude.  since that's not part of this script.
 
-# Function to generate the sweep arrays
-def generate_sweep(V1, V2, step):
-    sweep = None
-    if V1 == V2:
-        sweep = [V1]
-    elif (V2 >= 0 and V1 >= 0) or (V2 <= 0 and V1 <= 0):
-        if V1 < V2:
-            sweep = arange(V1, V2 + step, abs(step))
-        else:
-            sweep = arange(V1, V2 - step, -abs(step))
-    return sweep
-
 # Setup the sweep arrays
 print "--------------------------------------------------------"
 if T_sweep:
     print "Sweeping for {0} minutes, waiting for {1} seconds".format(Duration / 60.0, T_wait)
 else:
-    if SOURCE_1.get_range() < abs(V1_start) or SOURCE_1.get_range() < abs(V1_end):
-        print "WARING: Sweep outside SOURCE_1 range settings"
-    sweep_1 = generate_sweep(V1_start, V1_end, V1_step)
-    print "Sweep SOURCE_1 from " + str(V1_start) + "V to " + str(V1_end) + "V in " + str(V1_step) + "V steps"
+    print 'This script only does time sweeps, so you better set that up'
+    sys.exit(0)
 
 # Ask whether to continue
 print "--------------------------------------------------------"
@@ -215,6 +182,7 @@ if T_sweep:
     data.add_coordinate('Time [Unix]')
 data.add_coordinate(SOURCE_1_data_tag)
 data.add_value(MULTIMETER_data_tag)
+data.add_value("LockInX")
 data.create_file()
 
 plot_sweep = qt.Plot2D(data, name='Sweep', coorddim=1, valdim=2, maxtraces=4)
@@ -237,12 +205,16 @@ if T_sweep:
         if abs(I_meas) > 1e20:
             I_meas = 0
             break
-        data.add_data_point(time(), V, I_meas * I_sens / 1e-12)
+        data.add_data_point(time(), V, I_meas * I_sens / 1e-12, 0) # terrible terrible
         plot_sweep.update()
 
-        V = compensation()
+        V, LockInX = compensation(V)
+        if V > 0.5 or V < 0:
+            print 'exited bounds'
+            break
+        SOURCE_1.set_voltage(V)
         I_meas = MULTIMETER.get_readval()
-        data.add_data_point(time(), V, I_meas * I_sens / 1e-12)
+        data.add_data_point(time(), V, I_meas * I_sens / 1e-12, LockInX)
 
         if (I_meas * I_sens) > abs(I_max):
             print "BREAKING: Max I reached"
@@ -253,25 +225,7 @@ if T_sweep:
         qt.msleep(0.001)
         sleep(T_wait)  # probably blocking so not good, probably should use msleep
 
-else:
-    for V1 in sweep_1:
-        SOURCE_1.set_voltage(V1)
-        I_meas = MULTIMETER.get_readval()
-        # If MULTIMETER goes into overload set the value to zero
-        # This works for the HP multimeter, other multimeters may need another values
-        # surely you should do something else if it goes into overload,
-        # break/reset the instrument
-        if abs(I_meas) > 1e20:
-            I_meas = 0
-        data.add_data_point(V2, V1, I_meas * I_sens / 1e-12)
-        plot_sweep.update()
-        qt.msleep(0.001)
-        if (I_meas * I_sens) > abs(I_max):
-            print "BREAKING: Max I reached"
-            if enable_protection:
-                print 'Exceed Current Limit Exiting...'
-                break
-data.new_block()
+# data.new_block()
 # plot_map.update()
 
 qt.mend()
